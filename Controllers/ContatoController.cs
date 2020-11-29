3aa048efa865c025722fd748c1428ce20aea3b19
Lucas Batistao <lucassbatistao22@gmail.com>
2020-11-29 14:45:41 -0300

    Add funcionalidade de envio de email e alteração no layout

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +13,36 @@ namespace ProjetoHemobancoWeb.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string Nome, string Email, string Mensagem, string Assunto)
+        {
+            MailMessage message = new MailMessage();
+            SmtpClient smtp = new SmtpClient();
+            message.From = new MailAddress(Email);
+            message.To.Add(new MailAddress("seuemailaqui@gmail.com"));  //para qual email será enviado
+            message.Subject = "Cliente: " + Nome + " - " + "Assunto:" + Assunto;
+            message.IsBodyHtml = true; //to make message body as html  
+            message.Body = "Email: " + Email + "\n" + "Mensagem:" + Mensagem;
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com"; //for gmail host  
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("seuemailaqui@gmail.com", "suasenhaaqui");  //login e senha do seu email para aplicação enviar o email (precisa ser uma conta que permita aplicativos de terceiros usarem e sem autenticação de 2 fatores)
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+            try
+            {
+                smtp.Send(message);
+                ViewBag.lblMensagem = Nome + "," + '\n' + "mensagem enviada com sucesso!";
+            }
+            catch
+            {
+                ViewBag.lblMensagem = Nome + "," + '\n' + "erro ao enviar a mensagem!";
+            }
+           
             return View();
         }
     }
